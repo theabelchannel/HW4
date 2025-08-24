a7947cd713e06bc49a6fa3682159831bc618c038
Valery1302 <142460056+Valery1302@users.noreply.github.com>
2025-08-23 23:08:18 -0500

    Update SMSNotifier.java
    Mantienen implementación simple de envío.
    Solo ajustada para integrarse con la nueva NotifierFactory refactorizada.

--- src/main/java/com/sportspredictor/service/SMSNotifier.java
+++ src/main/java/com/sportspredictor/service/SMSNotifier.java
@@ -2,7 +2,9 @@ package com.sportspredictor.service;
 
 import com.sportspredictor.model.Cuenta;
 
+
 public class SMSNotifier implements INotifier {
+    
     @Override
     public void enviar(Cuenta destinatario, String mensaje) {
         System.out.println("Enviando SMS a: " + destinatario.getTelefono());
