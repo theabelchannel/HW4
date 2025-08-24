8d9aeb01ecb0715b96e77e41dbed8bb2bcb694dc
Valery1302 <142460056+Valery1302@users.noreply.github.com>
2025-08-23 22:56:49 -0500

    Update INotifierFactory.java
    Solo ajustados para integrarse con la nueva NotifierFactory refactorizada.

--- src/main/java/com/sportspredictor/service/INotifierFactory.java
+++ src/main/java/com/sportspredictor/service/INotifierFactory.java
@@ -1,7 +1,9 @@
 package com.sportspredictor.service;
 
+
 public interface INotifierFactory {
     INotifier crearEmailNotifier();
     INotifier crearSMSNotifier();
     INotifier crearPushNotifier();
 }
+
